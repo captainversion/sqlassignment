199029e97362bed7d6f87c1bcd32cbf15ce1369f
captainversion <136670723+captainversion@users.noreply.github.com>
2023-08-31 20:59:00 +0530

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,3 +1,16 @@
+Count the number of Salesperson whose name begin with ‘a’/’A’.
 
+count=1
+  
+Display all the Salesperson whose all orders worth is more than Rs. 2000.
+
+  
+Count the number of Salesperson belonging to Newyork.
+
+  
+Display the number of Salespeople belonging to London and belonging to Paris.
+
+  
+Display the number of orders taken by each Salesperson and their date of orders.  
 
 
