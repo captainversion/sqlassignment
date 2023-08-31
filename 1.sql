d0869700651a444b778c675f5a70f3d6b8db045c
captainversion <136670723+captainversion@users.noreply.github.com>
2023-08-31 21:02:33 +0530

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,16 +1,30 @@
-Count the number of Salesperson whose name begin with ‘a’/’A’.
+1.Count the number of Salesperson whose name begin with ‘a’/’A’.
 
 count=1
   
-Display all the Salesperson whose all orders worth is more than Rs. 2000.
+2.Display all the Salesperson whose all orders worth is more than Rs. 2000.
 
+1001 peel london 12.00
+1002 serres sanjose 13.00
   
-Count the number of Salesperson belonging to Newyork.
+3.Count the number of Salesperson belonging to Newyork.
 
+count=1
   
-Display the number of Salespeople belonging to London and belonging to Paris.
+4.Display the number of Salespeople belonging to London and belonging to Paris.
 
+London=2
   
-Display the number of orders taken by each Salesperson and their date of orders.  
+5.Display the number of orders taken by each Salesperson and their date of orders.  
 
+Axelrod	3009	1990-10-04
+Motika	3002	1990-10-03
+Peel	3003	1990-10-03
+Peel	3008	1990-10-05
+Peel	3011	1990-10-06
+Rifkin	3001	1990-10-03
+Rifkin	3006	1990-10-03
+Serres	3005	1990-10-03
+Serres	3007	1990-10-04
+Serres	3010	1990-10-06
 
